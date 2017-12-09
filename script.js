bbe5f810c4d78bfe12baca9ad3b7b501443938f3
Vincent <vincent.marlot95@gmail.com>
2017-12-09 04:21:52 +0100

    blop

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('coucou');
+console.log('coucou')
+
+
+var n = 6;
+var section = document.getElementById('game');
+var currentObstacle = 0;
+var currentCar = 0;
+
+for (var i=0; i < Math.pow(n,2); i++) {
+  var div = document.createElement('div');
+  div.setAttribute('id', i);
+  section.appendChild(div);
+}
+
+var currentObstacleX= getRandomArbitrary(0,5);
+var currentObstacleY= 0;
+var obstacle= document.createElement('img');
+img.setAttribute('src', "tay.jpg" );
+img.setAttribute('height', "20%" );
+img.setAttribute('width', "20%" );
+
+display(currentObstacleX , currentObstacleY, obstacle);
+
+function getRandomArbitrary(min, max) {
+
+  return Math.random() * (max - min) + min;
+}
+
+
+function display (x, y, element) {
+  //recuperation
+  document.getElementById(currentObstacle).innerHTML="";
+  var position = (y * n + x);
+  if (true === hasCollide()){
+    perdu();
+  } else {
+    document.getElementById(position).appendChild(element);
+    currentObstacle = position;
+  }
+}
+
+}
